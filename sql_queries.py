afb6f1c4fdadd1a3b954b5f43b5c3f25a2014d40
TekCatZ <TekCatZ.blog@gmail.com>
2022-02-11 09:26:40 +0700

    Add queries to transform data from stg

--- sql_queries.py
+++ sql_queries.py
@@ -10,6 +10,8 @@ config.read('dwh.cfg')
 staging_events_table_drop = "drop table if exists stg_events;"
 staging_songs_table_drop = "drop table if exists stg_songs;"
 songplay_table_drop = "drop table if exists songplays;"
+staging_user_table_drop = "drop table if exists stg_users;"
+staging_artist_table_drop = "drop table if exists stg_artists;"
 user_table_drop = "drop table if exists users;"
 song_table_drop = "drop table if exists songs;"
 artist_table_drop = "drop table if exists artists;"
@@ -17,7 +19,7 @@ time_table_drop = "drop table if exists time;"
 
 # CREATE TABLES
 
-staging_events_table_create= ("""
+staging_events_table_create = ("""
     create table if not exists stg_events (
         artist varchar,
         auth varchar,
@@ -57,7 +59,7 @@ staging_songs_table_create = ("""
 
 songplay_table_create = ("""
     create table if not exists songplays (
-        songplay_id serial primary key,
+        songplay_id int identity(1, 1) primary key,
         start_time  timestamp,
         user_id     int,
         level       varchar,
@@ -68,6 +70,17 @@ songplay_table_create = ("""
         user_agent  varchar,
         foreign key (user_id) references users(user_id),
         foreign key (start_time) references time(start_time)
+    );
+""")
+
+staging_user_table_create = ("""
+    create table if not exists stg_users (
+        user_id         int,
+        first_name      varchar,
+        last_name       varchar,
+        gender          varchar,
+        level           varchar,
+        last_updated    bigint
     );
 """)
 
@@ -88,6 +101,17 @@ song_table_create = ("""
         artist_id   varchar,
         year        int,
         duration    float
+    );
+""")
+
+staging_artist_table_create = ("""
+    create table if not exists stg_artists (
+        artist_id   varchar primary key,
+        name        varchar sortkey,
+        location    varchar,
+        latitude    float,
+        longitude   float,
+        year        int
     );
 """)
 
@@ -127,17 +151,67 @@ staging_songs_copy = ("""
     format as json 'auto ignorecase'
 """).format(config.get('IAM_ROLE', 'ARN'))
 
+staging_users_copy = ("""
+    insert into stg_users (
+        select distinct e.userId, e.firstName, e.lastName, e.gender, null, 0
+        from stg_events e
+        where e.page = 'NextSong'
+    );
+
+    update stg_users
+    set level = e.level, last_updated = e.ts
+    from stg_events e
+    where user_id = e.userId and e.page = 'NextSong'
+    and e.ts > last_updated;
+""")
+
+staging_artists_copy = ("""
+    insert into stg_artists (
+        select distinct s.artist_id, null, null, 0, 0, 0
+        from stg_songs s
+        where s.artist_id is not null
+    );
+
+    update stg_artists
+    set name = s.artist_name, location = s.artist_location, latitude = s.artist_latitude, longitude = s.artist_longitude
+    from stg_songs s
+    where stg_artists.artist_id = s.artist_id and s.year > stg_artists.year;
+""")
+
 # FINAL TABLES
 
 songplay_table_insert = ("""
+    insert into songplays (
+        start_time,
+        user_id,
+        level,
+        song_id,
+        artist_id,
+        session_id,
+        location,
+        user_agent
+    )
+    select
+        (TIMESTAMP 'epoch' + e.ts * INTERVAL '1 Second ') as start_time,
+        e.userId,
+        e.level,
+        s.song_id,
+        s.artist_id,
+        e.sessionId,
+        e.location,
+        e.userAgent
+    from stg_events e
+    left join songs s on e.song = s.title
+    left join artists a on e.artist = a.name
+    where e.page = 'NextSong';
 """)
 
 user_table_insert = ("""
     insert into users (
-        select userId, firstName, lastName, gender, level
-        from stg_events
-        where page = 'NextSong'
+        select user_id, first_name, last_name, gender, level
+        from stg_users
     );
+    drop table if exists stg_users;
 """)
 
 song_table_insert = ("""
@@ -149,9 +223,10 @@ song_table_insert = ("""
 
 artist_table_insert = ("""
     insert into artists (
-        select distinct artist_id, artist_name, artist_location, artist_latitude, artist_longitude
-        from stg_songs
+        select distinct artist_id, name, location, latitude, longitude
+        from stg_artists
     );
+    drop table if exists stg_artists;
 """)
 
 time_table_insert = ("""
@@ -171,7 +246,11 @@ time_table_insert = ("""
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
-drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
-copy_table_queries = [staging_events_copy, staging_songs_copy]
-insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+create_table_queries = [staging_events_table_create, staging_songs_table_create, staging_user_table_create,
+                        staging_artist_table_create, user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
+drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, staging_user_table_drop,
+                      staging_artist_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+copy_table_queries = [staging_events_copy, staging_songs_copy,
+                      staging_users_copy, staging_artists_copy]
+insert_table_queries = [song_table_insert, artist_table_insert,
+                        time_table_insert, user_table_insert, songplay_table_insert]
